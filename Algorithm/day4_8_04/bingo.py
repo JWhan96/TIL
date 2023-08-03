313cac7773c0128d7497f209a2ba029d717b90cd
jinwoo <chococan16@gmail.com>
2023-08-03 17:20:00 +0900

    8_03_2

--- Algorithm/day4_8_04/bingo.py
+++ Algorithm/day4_8_04/bingo.py
@@ -1,5 +1,5 @@
 #방향 배열
-from pprint import pprint
+# from pprint import pprint
 
 # arr = [list(map(int, input().split())) for _ in range(5)]
 # solut = []
@@ -56,6 +56,73 @@ for n in call:
         cnt += 1
     if di_list1[x + y] == 5 or di_list2[y - x + 4] == 5:
         cnt += 1
+    d= 1
     if cnt == 3:
         print(n)
+        break
+
+
+##### 승희 코드
+import sys
+
+sys.stdin = open('test.txt')
+input = sys.stdin.readline
+
+
+def check_num(num):
+    for i in range(5):
+        for j in range(5):
+            if board[i][j] == num:
+                board[i][j] = -1
+                return board
+
+
+def check_bingo():
+    bingo = 0
+    # row check
+    for i in range(5):
+        row = 0
+        for j in range(5):
+            if board[i][j] == -1:
+                row += 1
+        if row == 5:
+            bingo += 1
+    # col check
+    for j in range(5):
+        col = 0
+        for i in range(5):
+            if board[i][j] == -1:
+                col += 1
+        if col == 5:
+            bingo += 1
+
+    # diag check
+    diag1, diag2 = 0, 0
+    for i in range(5):
+        if board[i][i] == -1:
+            diag1 += 1
+    if diag1 == 5:
+        bingo += 1
+
+    for i in range(5):
+        if board[i][4 - i] == -1:
+            diag2 += 1
+    if diag2 == 5:
+        bingo += 1
+
+    return bingo
+
+
+board = [list(map(int, input().split())) for _ in range(5)]
+answer = []
+for i in range(5):
+    answer.extend(list(map(int, input().split())))
+
+# for num in answer[:13]: #어차피 13개 숫자까지는 빙고 절대 안 나오므로 그냥 체크
+#     check_num(num)
+
+for num in answer:  # 그 이후는 하나씩 체크하면서 빙고 계산
+    check_num(num)
+    if check_bingo() == 3:
+        print(num)
         break
