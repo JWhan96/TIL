2a70822bd7b2b0f66fe63998c431fb20fc3a213c
Hanjinwoo <chococan16@gmail.com>
2023-11-03 17:54:51 +0900

    11_03

--- PJT/PJT08/버전2_도전과제/pjt-08-skeleton/movies/views.py
+++ PJT/PJT08/버전2_도전과제/pjt-08-skeleton/movies/views.py
@@ -10,19 +10,27 @@ from .forms import MovieForm, GenreForm
 @require_http_methods(['GET'])
 def index(request):
     movies = Movie.objects.all()
-    genres = Genre.objects.all()
-    print(genres)
+    # genres = Genre.objects.all()
+    # print(genres)
     context = {
         'movies' : movies,
-        'genres' : genres
+        # 'genres' : genres
     }
     return render(request, 'movies/index.html', context)
 
 
 @require_safe
+@require_http_methods(['GET'])
 def detail(request, movie_pk):
-    pass
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    # genre = movie.genres.all()
+    
 
+    context = {
+        'movie' : movie,
+        # 'genre' : genre,
+    }
+    return render(request, 'movies/detail.html', context)
 
 @require_safe
 def recommended(request):
