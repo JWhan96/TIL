038a37beba5863ccae0169c544d89425c1e931e6
Hanjinwoo <chococan16@gmail.com>
2023-11-15 17:58:37 +0900

    11_15

---
+++ vue/08-02-vue-with-drf/vue-project/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <nav>
+      <RouterLink :to="{ name: 'ArticleView' }">Articles</RouterLink>
+      <RouterLink :to="{ name: 'SignUpView' }">SignUp</RouterLink>
+      <RouterLink :to="{ name: 'LogInView' }">LogIn</RouterLink>
+    </nav>
+  </header>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterView, RouterLink } from 'vue-router'
+</script>
+
+<style scoped>
+</style>
---
+++ vue/08-02-vue-with-drf/vue-project/src/views/SignUpView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>SignUp</h1>
+    <form @submit.prevent="signUp">
+      아이디: <input type="text" v-model.trim="username">
+      <br>
+      비밀번호: <input type="password" v-model.trim="password1">
+      <br>
+      비밀번호 확인: <input type="password" v-model.trim="password2">
+      <input type="submit">
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useCounterStore } from '@/stores/counter'
+
+const store = useCounterStore()
+const username = ref(null)
+const password1 = ref(null)
+const password2 = ref(null)
+
+
+const signUp = function () {
+  const payload = {
+    username: username.value,
+    password1: password1.value,
+    password2: password2.value,
+  }
+
+store.signUp(payload)
+}
+
+</script>
+
+<style>
+
+</style>
