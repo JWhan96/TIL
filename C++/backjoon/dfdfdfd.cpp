05d13123e406bb25e5d462c918391da93efa04f9
jinwoo <chococan16@gmail.com>
2024-02-05 16:11:35 +0900

    24_02_05

---
+++ C++/backjoon/dfdfdfd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+char ar4 = 'r';
+// char ar5 = "r"
+char arr[] = {'r', 'e', 'r', 'e', 'r', 'e',};
+// char arr[] = {"r", "e", "r", "e", "r", "e",};
+char ar3[] = "rerere";
+char ar2[] = "rerere";
+
+string att = "rere";
